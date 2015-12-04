edc942930f012393eb8ca07a03a442e9332b7ec9
olivif <olly2204@yahoo.com>
2015-12-04 23:43:46 +0100

    Day 2 solution.

--- test/test.js
+++ test/test.js
@@ -1,5 +1,6 @@
 var should = require("should");
 var main = require("./../lib/adventofcode");
+var fs = require('fs');
 
 describe("day1", function() {
 
@@ -39,4 +40,40 @@ describe("day1", function() {
 	done();
  });
  
+});
+
+describe("day2", function() {
+
+	it("should be able to calculate wrapping paper 1", function(done) {
+	
+		var output = main.getWrappingPaper(2, 3, 4);
+		output.should.equal(58);
+		
+		done();
+	});
+	
+	it("should be able to calculate wrapping paper 2", function(done) {
+	
+		var output = main.getWrappingPaper(1, 1, 10);
+		output.should.equal(43);
+
+		done();
+	});
+  
+  	it("should be able to calculate wrapping paper answer", function(done) {
+	
+		var array = fs.readFileSync('data/day2in.txt').toString().split("\n");
+		var total = 0;
+
+		for(var i in array) {
+			var input = array[i].split("x");
+			var output = main.getWrappingPaper(input[0], input[1], input[2]);
+			
+			total += output;
+		}
+		
+		total.should.equal(1588178);
+		done();
+	});
+	
 });
