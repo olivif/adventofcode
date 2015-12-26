d6b91600f3ebed366c60ad6de7d1842ee9331f65
olivif <olly2204@yahoo.com>
2015-12-26 12:15:47 +0100

    Splitting out files into days. Finishing day8 part A.

--- test/test.js
+++ test/test.js
@@ -1,6 +1,14 @@
 var should = require("should");
-var main = require("./../lib/adventofcode");
 var fs = require('fs');
+
+const day1 = require('./../lib/day1');
+const day2 = require('./../lib/day2');
+const day3 = require('./../lib/day3');
+const day4 = require('./../lib/day4');
+const day5 = require('./../lib/day5');
+const day6 = require('./../lib/day6');
+const day7 = require('./../lib/day7');
+const day8 = require('./../lib/day8');
 
 function runTestSuite(testCases, processCallback, comparisonCallback) {
 	for (var index = 0; index < testCases.length; index++) {
@@ -33,7 +41,7 @@ describe("day1", function() {
 			{ input: ")())())", output: -3 }
 		];
 		
-		runTestSuite(testCases, main.getFloors);
+		runTestSuite(testCases, day1.getFloors);
 		
 		done();
 	});
@@ -45,7 +53,7 @@ describe("day1", function() {
 			{ input: "()())", output: 5 }
 		];
 		
-		runTestSuite(testCases, main.getBasementIndex);
+		runTestSuite(testCases, day1.getBasementIndex);
 		
 		done();
 	});
@@ -53,7 +61,7 @@ describe("day1", function() {
 	it("should be able to count floors answer A", function(done) {
 	
 		var answerInput = fs.readFileSync('data/day1in.txt').toString();
-		var output = main.getFloors(answerInput);
+		var output = day1.getFloors(answerInput);
 		output.should.equal(280);
 		
 		console.log(output);
@@ -64,7 +72,7 @@ describe("day1", function() {
 	it("should be able to count floors answer B", function(done) {
 	
 		var answerInput = fs.readFileSync('data/day1in.txt').toString();
-		var output = main.getBasementIndex(answerInput);
+		var output = day1.getBasementIndex(answerInput);
 		output.should.equal(1797);
 		
 		console.log(output);
@@ -78,7 +86,7 @@ describe("day2", function() {
 
 	it("should be able to calculate wrapping paper A1", function(done) {
 	
-		var output = main.getWrappingPaper(2, 3, 4);
+		var output = day2.getWrappingPaper(2, 3, 4);
 		output.should.equal(58);
 		
 		done();
@@ -86,7 +94,7 @@ describe("day2", function() {
 	
 	it("should be able to calculate wrapping paper A2", function(done) {
 	
-		var output = main.getWrappingPaper(1, 1, 10);
+		var output = day2.getWrappingPaper(1, 1, 10);
 		output.should.equal(43);
 
 		done();
@@ -94,7 +102,7 @@ describe("day2", function() {
   
   	it("should be able to calculate wrapping paper B1", function(done) {
 	
-		var output = main.getRibbon(2, 3, 4);
+		var output = day2.getRibbon(2, 3, 4);
 		output.should.equal(34);
 		
 		done();
@@ -102,7 +110,7 @@ describe("day2", function() {
 	
 	it("should be able to calculate wrapping paper B2", function(done) {
 	
-		var output = main.getRibbon(1, 1, 10);
+		var output = day2.getRibbon(1, 1, 10);
 		output.should.equal(14);
 		
 		done();
@@ -115,7 +123,7 @@ describe("day2", function() {
 
 		for(var i in array) {
 			var input = array[i].split("x");
-			var output = main.getWrappingPaper(input[0], input[1], input[2]);
+			var output = day2.getWrappingPaper(input[0], input[1], input[2]);
 			
 			total += output;
 		}
@@ -131,7 +139,7 @@ describe("day2", function() {
 
 		for(var i in array) {
 			var input = array[i].split("x");
-			var output = main.getRibbon(input[0], input[1], input[2]);
+			var output = day2.getRibbon(input[0], input[1], input[2]);
 			
 			total += output;
 		}
@@ -152,7 +160,7 @@ describe("day3", function() {
 			{ input: "^v^v^v^v^v", output: 2 }
 		];
 		
-		runTestSuite(testCases, main.getHousesCount);
+		runTestSuite(testCases, day3.getHousesCount);
 		
 		done();
 	});
@@ -165,7 +173,7 @@ describe("day3", function() {
 			{ input: "^v^v^v^v^v", output: 11 }
 		];
 		
-		runTestSuite(testCases, main.getTwoSantasHouseCount);
+		runTestSuite(testCases, day3.getTwoSantasHouseCount);
 		
 		done();
 	});
@@ -173,7 +181,7 @@ describe("day3", function() {
 	it("should be able to count houses answer A", function(done) {
 	
 		var answerInput = fs.readFileSync('data/day3in.txt').toString();
-		var output = main.getHousesCount(answerInput);
+		var output = day3.getHousesCount(answerInput);
 		
 		output.should.equal(2592);
 		
@@ -183,7 +191,7 @@ describe("day3", function() {
 	it("should be able to count houses answer B", function(done) {
 	
 		var answerInput = fs.readFileSync('data/day3in.txt').toString();
-		var output = main.getTwoSantasHouseCount(answerInput);
+		var output = day3.getTwoSantasHouseCount(answerInput);
 		
 		output.should.equal(2360);
 		
@@ -199,7 +207,7 @@ describe("day4", function() {
 
 	xit("should be able to get salt A1", function(done) {
 	
-		var output = main.getHashSalt("abcdef", zeroes5);
+		var output = day4.getHashSalt("abcdef", zeroes5);
 		output.should.equal(609043);
 		
 		done();
@@ -207,7 +215,7 @@ describe("day4", function() {
 	
 	xit("should be able to get salt A2", function(done) {
 	
-		var output = main.getHashSalt("pqrstuv", zeroes5);
+		var output = day4.getHashSalt("pqrstuv", zeroes5);
 		output.should.equal(1048970);
 		
 		done();
@@ -215,7 +223,7 @@ describe("day4", function() {
 	
 	xit("should be able to get salt answer A", function(done) {
 	
-		var output = main.getHashSalt("yzbqklnj", zeroes5);
+		var output = day4.getHashSalt("yzbqklnj", zeroes5);
 		output.should.equal(282749);
 		
 		done();
@@ -223,7 +231,7 @@ describe("day4", function() {
 	
 	xit("should be able to get salt answer B", function(done) {
 	
-		var output = main.getHashSalt("yzbqklnj", zeroes6);
+		var output = day4.getHashSalt("yzbqklnj", zeroes6);
 		output.should.equal(9962624);
 		
 		done();
@@ -243,7 +251,7 @@ describe("day5", function() {
 			{ input: "dvszwmarrgswjxmb", output: false }
 		];
 	
-		runTestSuite(testCases, main.isNiceString);
+		runTestSuite(testCases, day5.isNiceString);
 		
 		done();
 	});
@@ -257,7 +265,7 @@ describe("day5", function() {
 			{ input: "ieodomkazucvgmuy", output: false }
 		];
 	
-		runTestSuite(testCases, main.isNiceStringV2);
+		runTestSuite(testCases, day5.isNiceStringV2);
 		
 		done();
 	});
@@ -269,7 +277,7 @@ describe("day5", function() {
 
 		for(var i in array) {
 			var input = array[i];
-			var output = main.isNiceString(input);
+			var output = day5.isNiceString(input);
 			
 			if (output ) {
 				total++;
@@ -287,7 +295,7 @@ describe("day5", function() {
 
 		for(var i in array) {
 			var input = array[i];
-			var output = main.isNiceStringV2(input);
+			var output = day5.isNiceStringV2(input);
 			
 			if (output) {
 				total++;
@@ -309,7 +317,7 @@ describe("day6", function() {
 			{ input: "toggle 294,259 through 474,326", output: "toggle" },
 		];
 	
-		runTestSuite(testCases, main.parseInstruction, function(actualOutput, expectedOutput) {
+		runTestSuite(testCases, day6.parseInstruction, function(actualOutput, expectedOutput) {
 			return expectedOutput.should.equal(actualOutput.type);
 		});
 				
@@ -324,7 +332,7 @@ describe("day6", function() {
 			{ input: "toggle 294,259 through 474,326", output: {topX: 294, topY: 259} },
 		];
 	
-		runTestSuite(testCases, main.parseInstruction, function(actualOutput, expectedOutput) {
+		runTestSuite(testCases, day6.parseInstruction, function(actualOutput, expectedOutput) {
 			return expectedOutput.topX.should.equal(actualOutput.topX);
 			return expectedOutput.topY.should.equal(actualOutput.topY);
 		});
@@ -340,7 +348,7 @@ describe("day6", function() {
 			{ input: "toggle 294,259 through 474,326", output: {bottomX: 474, bottomY: 326} },
 		];
 	
-		runTestSuite(testCases, main.parseInstruction, function(actualOutput, expectedOutput) {
+		runTestSuite(testCases, day6.parseInstruction, function(actualOutput, expectedOutput) {
 			return expectedOutput.bottomX.should.equal(actualOutput.bottomX);
 			return expectedOutput.bottomY.should.equal(actualOutput.bottomY);
 		});
@@ -356,7 +364,7 @@ describe("day6", function() {
 			{ input: ["turn off 499,499 through 500,500"], output: 0 },
 		];
 	
-		runTestSuite(testCases, main.runLightsInstructionsA);
+		runTestSuite(testCases, day6.runLightsInstructionsA);
 				
 		done();
 	});
@@ -365,7 +373,7 @@ describe("day6", function() {
 	
 		var array = fs.readFileSync('data/day6in.txt').toString().split("\n");
 	
-		var output = main.runLightsInstructionsA(array);
+		var output = day6.runLightsInstructionsA(array);
 		output.should.equal(377891);
 				
 		done();
@@ -375,7 +383,7 @@ describe("day6", function() {
 	
 		var array = fs.readFileSync('data/day6in.txt').toString().split("\n");
 	
-		var output = main.runLightsInstructionsB(array);
+		var output = day6.runLightsInstructionsB(array);
 		output.should.equal(14110788);
 				
 		done();
@@ -405,7 +413,7 @@ describe("day7", function() {
 		
 		runTestSuite(testCases, 
 			function(input) {
-				return main.parseAssignmentOperation(input, registry);
+				return day7.parseAssignmentOperation(input, registry);
 			}, 
 			function(actualOutput, expectedOutput) {
 				return expectedOutput.key.should.equal(actualOutput.key);
@@ -418,7 +426,7 @@ describe("day7", function() {
 	
 	it("should be able to run list of ops", function(done) {
 		
-		var registry = main.runAssignmentOperations("data/day7in.txt");
+		var registry = day7.runAssignmentOperations("data/day7in.txt");
 		
 		registry["d"].should.equal(72);
 		registry["e"].should.equal(507);
@@ -434,7 +442,7 @@ describe("day7", function() {
 	
 	it("should be able to run list of ops answer A", function(done) {
 		
-		var registry = main.runAssignmentOperations("data/day7in2.txt");
+		var registry = day7.runAssignmentOperations("data/day7in2.txt");
 
 		registry["a"].should.equal(3176);
 				
@@ -443,7 +451,7 @@ describe("day7", function() {
 	
 	it("should be able to run list of ops answer B", function(done) {
 		
-		var registry = main.runAssignmentOperations("data/day7in3.txt");
+		var registry = day7.runAssignmentOperations("data/day7in3.txt");
 
 		registry["a"].should.equal(14710);
 				
@@ -455,48 +463,47 @@ describe("day8", function() {
 
 	it("should be able to get string length", function(done) {
 		var testCases = [
-			{ input: "", output: 0},
-			{ input: "abc", output: 3},
-			{ input: "aaa\"aaa", output: 7},
-			{ input: "\x27", output: 1},
+			{ input: '""', output: 0},
+			{ input: '"abc"', output: 3},
+			{ input: '"aaa\\"aaa"', output: 7},
+			{ input: '"\\x27"', output: 1},
 		];
 		
-		runTestSuite(testCases, main.getStringTotalLength);
+		runTestSuite(testCases, day8.getStringCodeLength);
 		
 		done();
 	});
 	
-	xit("should be able to get string code length", function(done) {
+	it("should be able to get string code length", function(done) {
 		var testCases = [
-			{ input: "", output: 2},
-			{ input: "abc", output: 5},
-			{ input: "aaa\"aaa", output: 10},
-			{ input: "\x27", output: 6},
+			{ input: '""', output: 2},
+			{ input: '"abc"', output: 5},
+			{ input: '"aaa\\"aaa"', output: 10},
+			{ input: '"\\x27"', output: 6},
 		];
 		
-		runTestSuite(testCases, main.getStringCodeLength);
+		runTestSuite(testCases, day8.getStringTotalLength);
 		
 		done();
 	});
 	
-	xit("should be able to get string code answer A", function(done) {
+	it("should be able to get string code answer A", function(done) {
 		
 		var fileName = 'data/day8in.txt';
-		var array = fs.readFileSync(fileName, "utf8").split("\n");
-		var fileStats = fs.statSync(fileName);
+		var array = fs.readFileSync(fileName, "utf8").split("\r\n");
 		var totalLength = 0;
+		var codeLength = 0;
 
 		for(var i in array) {
 			var input = array[i];
-			totalLength += main.getStringTotalLength(input);
+			totalLength += day8.getStringTotalLength(input);
+			codeLength += day8.getStringCodeLength(input);
 		}
 		
-		console.log(fileStats.size + " " + totalLength);
-		var total = fileStats.size - totalLength;
+		var total = totalLength - codeLength;
 		
-		total.should.equal(0);
+		total.should.equal(1371);
 		done();
 	});
-	
 });
 	
